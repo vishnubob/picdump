d73362fbcf5ef63f4ecbc417eaedca316dbf6b6e
Giles Hall <giles@polymerase.org>
2014-11-13 16:56:35 -0500

    expansion of command line options (hardlink, symlink, etc)

--- picdump.py
+++ picdump.py
@@ -16,11 +16,11 @@ class ImageRoll(list):
         self.root = root
         super(ImageRoll, self).__init__(roll)
 
-    def copy_roll(self, symlink=False):
+    def copy_roll(self, copy_mode=None):
         if not os.path.exists(self.root):
             os.makedirs(self.root)
         for image in self:
-            image.copy_image(self.root, symlink)
+            image.copy_image(self.root, copy_mode=copy_mode)
 
 class Image(object):
     EXIF_Camera_Model = "Image Model"
@@ -28,7 +28,7 @@ class Image(object):
     EXIF_Timestamp_Format = "%Y:%m:%d %H:%M:%S"
 
     def __init__(self, imgfn):
-        self.imgfn = imgfn
+        self.imgfn = os.path.abspath(imgfn)
         (stem, ext) = os.path.splitext(imgfn)
         self.ext = ext.lower()
         if ext == ".jpeg":
@@ -48,22 +48,32 @@ class Image(object):
     def __cmp__(self, other):
         return cmp(self.timestamp, other.timestamp)
     
-    def copy_image(self, root, symlink=False):
+    def copy_image(self, root, copy_mode=None):
+        if copy_mode not in ("hardlink", "symlink"):
+            copy_mode = "copy"
         fn = os.path.split(self.imgfn)[-1]
         dest = os.path.join(root, fn)
-        os.path.copyfile(self.imgfn, dest)
-        print self.imgfn, "->", dest
+        msg = "%s -> %s (%s)" % (self.imgfn, dest, copy_mode)
+        print msg
+        if copy_mode == "symlink":
+            os.symlink(self.imgfn, dest)
+        elif copy_mode == "hardlink":
+            os.link(self.imgfn, dest)
+        else:
+            shutil.copyfile(self.imgfn, dest)
 
 class PicDump(object):
     def __init__(self, args):
         self.import_dir = args.import_dir
         self.export_dir = args.export_dir
         self.roll_threshold = pd.Timedelta(minutes=args.roll_threshold)
+        self.roll_label = args.roll_label
+        self.copy_mode = args.copy_mode
 
     def process(self):
         images = self.get_images()
         for roll in self.separate_rolls(images):
-            roll.copy_roll()
+            roll.copy_roll(copy_mode=self.copy_mode)
     
     def get_images(self):
         images = []
@@ -89,7 +99,7 @@ class PicDump(object):
                 rolls = [val for (key, val) in df.groupby(cluster)]
                 for roll in rolls:
                     roll = roll.sort("timestamp")
-                    ts = roll.iloc[0]["timestamp"].strftime("%Y-%m-%d %H:%M:%S")
+                    ts = roll.iloc[0]["timestamp"].strftime(self.roll_label)
                     _ext = ext
                     while _ext[0] == '.':
                         _ext = _ext[1:]
@@ -103,13 +113,18 @@ Defaults = {
     'import_dir': '.',
     'export_dir': '.',
     'roll_threshold': 60,
+    'roll_label': "%Y-%m-%d_%H%M",
+    'copy_mode': "copy",
 }
 
 def get_cli():
     parser = argparse.ArgumentParser(description='picdump')
     parser.add_argument('-i', '--import', dest="import_dir", help='Root of directory to import pictures from')
     parser.add_argument('-o', '--export', dest="export_dir", help='Root of the directory to copy pictures to')
-    parser.add_argument('-t', '--threshold', dest="roll_threshold", help='Threshold in minutes to separate film rolls by')
+    parser.add_argument('-t', '--threshold', type=int, dest="roll_threshold", help='Threshold in minutes to separate film rolls by')
+    parser.add_argument('-L', '--roll-label', dest="roll_label", help='Roll label (see strftime() for format')
+    parser.add_argument('--symlink', action='store_const', const="symlink", dest="copy_mode", help='Instead of copying files, make a symlink')
+    parser.add_argument('--hardlink', action='store_const', const="hardlink", dest="copy_mode", help='Instead of copying files, make a hardlink')
     parser.set_defaults(**Defaults)
     args = parser.parse_args()
     return args
